cabd6db4e387f61292914a6f2eb00ace6d618768
marc_go <goringm64@gmail.com>
2025-03-27 09:36:47 +0000

    Fix Bug

--- htdocs/assets/md3.php
+++ htdocs/assets/md3.php
@@ -9,8 +9,8 @@ echo '
     }
 </script>
 <script type="module">
-    import '@material/web/all.js';
-    import {styles as typescaleStyles} from '@material/web/typography/md-typescale-styles.js';
+    import "@material/web/all.js";
+    import {styles as typescaleStyles} from "@material/web/typography/md-typescale-styles.js";
 
     document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
 </script>
