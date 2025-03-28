db0e96bb3d414d210d26ffb87af2b454c5fc4c25
marc_go <goringm64@gmail.com>
2025-03-28 17:58:29 +0000

    Add Menue

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,13 +1,25 @@
 <?php
-define("PATH", "");
+/* define("PATH", "");
 
 require "assets/admin.php";
 $session = new loginManager();
 if (!$session->checkLogin()) {
 	header("Location: login.php");
 	exit;
-}else{
-	header ("Location: apps/home");
-	exit;
-}
+} */
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>470Cloud</title>
+</head>
+<body>
+<md-navigation-bar>
+        <md-navigation-tab label="Home"></md-navigation-tab>
+        <md-navigation-tab label="Über uns"></md-navigation-tab>
+        <md-navigation-tab label="Kontakt"></md-navigation-tab>
+    </md-navigation-bar>
+</body>
+</html>
