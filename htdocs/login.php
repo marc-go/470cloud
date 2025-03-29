46e374b6ad46bba09f5617919823d486495a8893
marc_go <goringm64@gmail.com>
2025-03-29 17:17:54 +0000

    Change Redirect

--- htdocs/login.php
+++ htdocs/login.php
@@ -63,15 +63,13 @@ if (isset($_POST["user"]) && isset($_POST["pw"])) {
             echo "Fehler beim Setzen der Cookies.";
         }
 
-        /* if (isset($_POST["from"])) {
+        if (isset($_POST["from"])) {
             header("Location: " . $_POST["from"]);
             exit;
         } else {
             header("Location: apps/home");
             exit;
-        } */
-       header("Location: index.php");
-       exit;
+        }
     }
 }
 ?>
