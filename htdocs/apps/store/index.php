aa55deee78e79994738f70309e689e16fd9ab9fb
marc-go <goringm64@gmail.com>
2025-06-09 19:06:04 +0200

    Add App Store

--- htdocs/apps/store/index.php
+++ htdocs/apps/store/index.php
@@ -1,3 +1,16 @@
+<?php
+require $_SERVER["DOCUMENT_ROOT"] . "/assets/admin.php";
+
+$session = new loginManager();
+if (!$session->checkLogin()) {
+    header("Location: /login.php?from=apps/store");
+    exit;
+}
+
+if (!$session->getAdmin()) {
+    echo "Premmision Denied: You are not an admin.";
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,6 +25,16 @@
     <link rel="prefetch" href="/apps/settings">
 </head>
 <body>
-    
+    <?php require $_SERVER["DOCUMENT_ROOT"] . "/assets/menue.php"; ?>
+    <md-list>
+        <?php
+        $file = file_get_contents("https://api.470cloud.marc-goering.de/app/cloud/app-store/all.json");
+        $file = json_decode($file, true);
+
+        foreach ($file as $id => $json) {
+           echo '<md-list-item type="link" onclick="info(' . $id . ')">' . $json["name"] . '</md-list-item>';
+        }
+        ?>
+    </md-list>
 </body>
 </html>
